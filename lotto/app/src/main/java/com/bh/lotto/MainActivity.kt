0de0a664c2907867e259757036a6bb24494ab611
brian <brianhwang29@gmail.com>
2021-05-19 12:01:33 +0900

    modified

--- lotto/app/src/main/java/com/bh/lotto/MainActivity.kt
+++ lotto/app/src/main/java/com/bh/lotto/MainActivity.kt
@@ -2,10 +2,43 @@ package com.bh.lotto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val lotteryNumbs = arrayListOf(number1, number2, number3, number4, number5, number6)
+
+        val countDownTimer = object: CountDownTimer(2000,100){
+            override fun onTick(millisUntilFinished: Long) {
+                lotteryNumbs.forEach{
+                    val randomNumber = (Math.random() * 45 + 1).toInt()
+                    it.text = "$randomNumber"
+                }
+            }
+
+            override fun onFinish() {
+            }
+        }
+
+        lotteryBtn.setOnClickListener{
+            /* moved to the timer ontop
+            lotteryNumbs.forEach{
+                val randomNumber = (Math.random() * 45 + 1).toInt()
+                it.text = "$randomNumber"
+            }
+            */
+
+            if(lotteryBtn.isAnimating){
+                lotteryBtn.cancelAnimation()
+                countDownTimer.cancel()
+            } else {
+                lotteryBtn.playAnimation()
+                countDownTimer.start()
+            }
+        }
     }
 }
--- lotto/app/src/main/java/com/bh/lotto/SplashActivity.kt
+++ lotto/app/src/main/java/com/bh/lotto/SplashActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity() {
@@ -11,16 +12,19 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        aniView.setOnClickListener{
+        val handler = Handler(Looper.getMainLooper())
+        val runnable = Runnable {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
         }
+        handler.postDelayed(runnable, 3000)
 
-        Handler().postDelayed(Runnable {
+        aniView.setOnClickListener{
+            handler.removeCallbacks(runnable)
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
-        }, 3000)
+        }
     }
 }
