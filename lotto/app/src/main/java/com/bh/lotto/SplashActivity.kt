a70b892a1a3314f282283bfd23b85e2f5e5ff358
brian <brianhwang29@gmail.com>
2021-05-19 11:21:04 +0900

    main layout setup

---
+++ lotto/app/src/main/java/com/bh/lotto/SplashActivity.kt
@@ -0,0 +1,26 @@
+package com.bh.lotto
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import kotlinx.android.synthetic.main.activity_splash.*
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        aniView.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        Handler().postDelayed(Runnable {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
